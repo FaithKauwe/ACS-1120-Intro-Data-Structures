94e59b08b481baf58b19c17120e4a25a9e05e07a
FaithKauwe <fkauwe@gmail.com>
2025-02-10 19:04:32 -0800

    random word with sample text generating to local browser

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,6 @@
 """Main script, uses other modules to generate sentences."""
 from flask import Flask
+from dictogram import Dictogram
 
 
 app = Flask(__name__)
@@ -7,11 +8,15 @@ app = Flask(__name__)
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
 
+fish_text = 'one fish two fish red fish blue fish'
+histogram = Dictogram(fish_text.split())
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    return "<p>Hello World!</p>"
+    random_word = histogram.sample()
+    return f"<p>Random word: {random_word}</p>"
+
 
 
 if __name__ == "__main__":
